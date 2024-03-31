a1b0b5e6669b88ab4417eb9466206f158cb07522
Kurai2610 <monroytorresdavidfernando26@gmail.com>
2024-03-31 12:55:00 -0500

    secret key configuration and debugging mode added

--- server/core/settings.py
+++ server/core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-66qodp(38d(#2x8qc4!j!1b+)1--zp$+@()i&)hl6g&g9a(s6h'
+SECRET_KEY = config('SECRET_KEY', default='insecurekey-please-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
